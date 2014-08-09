74fa772a9bd32ff69c1b84174a5778e654f5f7b2
akihatake <akihatake@gmail.com>
2014-08-10 00:12:41 +0900

    神経衰弱として最低限の動作まで

--- js/main.js
+++ js/main.js
@@ -7,6 +7,9 @@ var scene;
 var man;
 var cards = new Array();
 var cardsGroup = new Group();
+var openedCard;
+var correctNum = 0;
+var lifeGage;
 
 // 定数
 var SCREEN_WIDTH = 800;
@@ -16,98 +19,88 @@ var CARD_HEIGHT = 73;
 var CARD_NUM = 8;
 var MAN_WIDTH = 460;
 var MAN_HEIGHT = 257;
+var MAX_LIFE = 20;
 
 var cardStatus = {
     close: 0,
     action: 1,
     open: 2,
-}
-
-//
-// 乱数生成
-//
-function rand(n) {
-    return Math.floor(Math.random() * (n + 1));
+    finish: 3,
 }
 
 
 /********************************
- ゲーム内の要素をクラス定義
+ クラス定義
  ********************************/
-//
-// 基本カードクラス
-//
-// 　共通する状態フラグだったりイベント処理を定義して、必要であれば
-// 　これを継承してカードオブジェクトを作る
-var Card = Class.create(Sprite, {
+/*
+ * 基本カードクラス
+ */
+Card = Class.create(Sprite, {
     // コンストラクタ
     initialize: function(x, y) {
         Sprite.call(this, CARD_WIDTH, CARD_HEIGHT);
-        this.image = game.assets['img/card_ura.png'];
+        this.image = game.assets['img/cards.png'];
         this.x = x;
         this.y = y;
         this.frame = 0;
         this.fps = 30;
         this.status = cardStatus.close;
-
         this.num = 0;
-        this.origX = x;
-        this.origY = y;
-        this.vibration = false;
     },
 
+    // カードめくる
+    open: function() {
+        this.frame = this.num;
+        this.status = cardStatus.open;
+    },
+
+    // カード閉じる
+    close: function() {
+    	this.frame = 0;
+        this.status = cardStatus.close;
+	},
+    
     // タッチスタートイベント
     ontouchstart: function(e) {
         switch(this.status) {
             case cardStatus.close:
-                this.visible = false;
-                man.actionFlg = true;
-                this.status = cardStatus.action;
-                break;
-            case cardStatus.open:
-                this.status = cardStatus.close;
-                this.image = game.assets['img/card_ura.png'];
+                this.tl.scaled
+       			this.open();
+        		man.actionFlg = true;
                 break;
             default:
                 break;
         }
     },
-
+    
     // タッチエンドイベント
-    ontouchend: function(e) { 
-        this.visible = true;
-        man.actionFlg = false;
-
-        switch(this.status) {
-            case cardStatus.action:
-                this.status = cardStatus.open;
-                this.image = game.assets['img/card00.png'];
-                break;
-            default:
-                break;
+    ontouchend: function(e) {
+        if (this.status == cardStatus.open)
+        {
+            judge(this);
+            man.actionFlg = false;
         }
     },
-
+    
     // フレームイベント
     onenterframe: function(e) {
-        if (this.vibration)
-        {
-            if (game.frame % 2 == 0) {
-                this.moveTo(this.x, this.y - 10);
-            }
-            else {
-                this.moveTo(this.x, this.y + 10);
-            }
-        }
+//        if (this.vibration)
+//        {
+//            if (game.frame % 2 == 0) {
+//                this.moveTo(this.x, this.y - 10);
+//            }
+//            else {
+//                this.moveTo(this.x, this.y + 10);
+//            }
+//        }
     }
 });
 
 
-//
-// 人クラス
-//
-// 　カードをめくった時にアニメーションさせたりします
-var Man = Class.create(Sprite, {
+/*
+ * 人クラス
+ */
+Man = Class.create(Sprite, {
     // コンストラクタ
     initialize: function(x, y) {
         Sprite.call(this, MAN_WIDTH, MAN_HEIGHT);
@@ -147,46 +140,28 @@ var Man = Class.create(Sprite, {
     }
 
 });
-
-
-//
-// メータークラス
-//
-// 　はずれるとメーターがどんどん上がる
-var Meter = Class.create(Sprite, {
-    // コンストラクタ
-    initialize: function(x, y) {
-
-    },
-
-    // フレームイベント
-    onenterframe: function(e) {
-
-    }
-
-});
     
 
 
-
-// メイン処理
+/********************************
+ メイン処理
+ ********************************/
 window.onload = function () {
     //
-    game = new Game(SCREEN_WIDTH, SCREEN_HEIGHT);
+    game = new Core(SCREEN_WIDTH, SCREEN_HEIGHT);
     scene = game.rootScene;
 
     //
-    game.fps = 15;
+    game.fps = 30;
     game.preload('img/kabe.png'
                  , 'img/table.png'
-                 , 'img/card_ura.png'
+                 , 'img/cards.png'
                  , 'img/esper00.png'
                  , 'img/esper01.png'
                  , 'img/esper02.png'
                  , 'img/esper03.png'
                  , 'img/esper04.png'
-                 , 'img/card00.png'
-                 , 'img/card01.png'
+                 , 'img/bar.png'
                 );
     game.onload = function () {
         //
@@ -199,12 +174,20 @@ window.onload = function () {
         bg1.image = game.assets['img/kabe.png'];
         bg2.image = game.assets['img/table.png'];
 
+
+        // ライフゲージ
+        lifeGage = new LifeLabel(32, 32, MAX_LIFE);
+        lifeGage.life = MAX_LIFE;
+//        var score = new ScoreLabel(32, 54);
+        
 		// 画面表示（後から指定した方が上に重なる）
         scene.addChild(bg1);
         scene.addChild(man);
         scene.addChild(bg2);
+        scene.addChild(lifeGage);
+//        scene.addChild(score);
 
-        
+        // カードの初期化
         initCards();
     }
 
@@ -213,26 +196,63 @@ window.onload = function () {
 
 
 
+/********************************
+ 関数
+ ********************************/
 // カードの初期化
 function initCards() {
 	var num;
-    var cardIndex;
+    var index;
     
     // カード生成
     for (var i = 0; i < CARD_NUM; i++) {
-//        num = Math.floor(i / 2);
-//        do {
-//	        cardIndex = Math.floor(Math.random() * CARD_NUM);
-//        } while(cards[cardIndex] != undefined);
-        cards[i] = new Card((CARD_WIDTH + 20) * i, 0);
-        cards[i].num = Math.floor(i / 2);
+        num = 1 + Math.floor(i / 2);
+        do {
+	       index = rand(CARD_NUM);
+        } while(typeof cards[index] !== 'undefined');
+        var card = new Card(0, 0);
+        card.x = (card.width + 20) * index;
+        cards[index] = card;
+        cards[index].num = num;
+        cardsGroup.addChild(cards[index]);
 
-        console.log("i=" + i);
-        console.log("num=" + cards[i].num);
-
-        cardsGroup.addChild(cards[i]);
+        //
+        console.log("i=" + i + "/num=" + cards[index].num + "/index=" + index);
     }
     cardsGroup.x = 80;
     cardsGroup.y = 320;
     scene.addChild(cardsGroup);
 };
+
+
+
+
+// カード判定
+function judge(card) {
+    if (typeof openedCard === 'undefined') {
+        // 1枚目
+        openedCard = card;
+    } else {
+        // 2枚目
+	    if (openedCard.num == card.num) {
+            openedCard.status = cardStatus.finish;
+            card.status = cardStatus.finish;
+        	correctNum++;
+	        if (correctNum == CARD_NUM / 2) {
+    	        alert("your score is ..");
+        	}
+        }
+        else
+        {
+            lifeGage.life -= 2;
+			console.log("life=" + lifeGage.life);
+            openedCard.close();
+            card.close();
+
+            if (lifeGage.life < 1)
+        		alert("Game Over!")
+        }
+        
+        openedCard = undefined;
+    }
+}
